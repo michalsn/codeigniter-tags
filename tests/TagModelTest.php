556abc26608dc089164fdbe33f5012b48cdd8bed
michalsn <michal@sniatala.pl>
2023-10-24 20:36:00 +0200

    get tags by taggable_type

--- tests/TagModelTest.php
+++ tests/TagModelTest.php
@@ -59,4 +59,38 @@ final class TagModelTest extends TestCase
 
         $this->assertCount(0, $result);
     }
+
+    public function testFindByTypes()
+    {
+        $result = $this->tagModel->findByTypes(['countries']);
+
+        $this->assertCount(2, $result);
+        $this->assertSame('Portugal', $result[0]->name);
+        $this->assertSame('Italy', $result[1]->name);
+    }
+
+    public function testFindByTypesMany()
+    {
+        $result = $this->tagModel->findByTypes(['countries', 'foods']);
+
+        $this->assertCount(4, $result);
+        $this->assertSame('Portugal', $result[0]->name);
+        $this->assertSame('Italy', $result[1]->name);
+        $this->assertSame('Carrot', $result[2]->name);
+        $this->assertSame('Potato', $result[3]->name);
+    }
+
+    public function testFindByTypesNotExist()
+    {
+        $result = $this->tagModel->findByTypes(['not-exist']);
+
+        $this->assertCount(0, $result);
+    }
+
+    public function testFindByTypesEmpty()
+    {
+        $result = $this->tagModel->findByTypes([]);
+
+        $this->assertCount(0, $result);
+    }
 }
